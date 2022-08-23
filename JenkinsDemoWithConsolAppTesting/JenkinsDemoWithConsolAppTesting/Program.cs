fdf76228a1bad742b7305acd65c7654ba13cf8f8
nagaraju onteru <nagaraju_onteru@epam.com>
2022-08-23 21:15:30 +0530

    Intial Update

---
+++ JenkinsDemoWithConsolAppTesting/JenkinsDemoWithConsolAppTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace JenkinsDemoWithConsoleAppTesting
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
