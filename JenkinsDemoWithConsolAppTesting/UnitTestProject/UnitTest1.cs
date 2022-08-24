41a0e4c8d8cb9e078dd3df7130be52ee4a8ef5c3
nagaraju onteru <nagaraju_onteru@epam.com>
2022-08-24 17:03:51 +0530

    Checking for test fail

--- JenkinsDemoWithConsolAppTesting/UnitTestProject/UnitTest1.cs
+++ JenkinsDemoWithConsolAppTesting/UnitTestProject/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace UnitTestProject
     [TestClass]
     public class UnitTest1
     {
-        private const string Expected = "Hello World!";
+        private const string Expected = "Hello World";
         [TestMethod]
         public void TestMethod1()
         {
