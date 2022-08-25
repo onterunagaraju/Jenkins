f99b71d07ab3c47f1ade7b3306cc51102ae5022c
nagaraju onteru <nagaraju_onteru@epam.com>
2022-08-25 09:16:55 +0530

    Added Some un used lines

--- JenkinsDemoWithConsolAppTesting/JenkinsDemoWithConsolAppTesting/Program.cs
+++ JenkinsDemoWithConsolAppTesting/JenkinsDemoWithConsolAppTesting/Program.cs
@@ -3,6 +3,9 @@ namespace JenkinsDemoWithConsoleAppTesting
 {
     public class Program
     {
+        public int a;
+        public int b = 2;
+       
         public static void Main()
         {
             Console.WriteLine("Hello World!");
